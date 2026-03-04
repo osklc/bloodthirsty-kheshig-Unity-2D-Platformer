57b72653766870bb633ab4c3d7ede067cd1036d6
osklc <osklcmail@gmail.com>
2026-03-05 01:27:24 +0300

    Enhance player mechanics: increase gravity scale, adjust jump force, and implement attack functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,14 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     private Rigidbody2D rb;
     private float moveInput;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 20;
+
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();    
@@ -25,10 +33,39 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }   
+
+        if (Time.time >= nextAttackTime)
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y); 
     }
+
+    void Attack()
+    {
+        Debug.Log("Kheshig attack!"); //for debug
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Vurulan Dusman: " + enemy.name);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 }
